21d17cdad6508fc582aa28b64f50325a273715b0
Maurício Braz Gonçalves <82123955+mauriciobraz@users.noreply.github.com>
2024-04-17 00:01:40 -0400

    feat(web): markdown component

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
+  plugins: [require("@tailwindcss/typography")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
+    },
   },
 };
